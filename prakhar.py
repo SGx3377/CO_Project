3f54e177ed7f84bb7af2d8e53f7721ca9c68913d
VIRUSXSUJAL69 <prakhar22361@iiitd.ac.in>
2023-04-27 20:13:18 +0530

    I fixed the mov operation

--- prakhar.py
+++ prakhar.py
@@ -1,12 +1,12 @@
-# dict_opcodes = {"add":"00000", "sub":"00001", "mov":["00010","00011"], "ld":"00100",
-#         "st":"00101", "mul":"00110", "div":"00111", "rs":"01000",
-#         "ls":"01001", "xor":"01010", "or":"01011", "and":"01100",
-#         "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"11100", "jgt":"11101", "je":"11111", "hlt":"11010"}
-
-dict_opcodes = {"add":"00000", "sub":"00001", "mov":"00011", "ld":"00100",
+dict_opcodes = {"add":"00000", "sub":"00001", "mov":["00010","00011"], "ld":"00100",
         "st":"00101", "mul":"00110", "div":"00111", "rs":"01000",
         "ls":"01001", "xor":"01010", "or":"01011", "and":"01100",
         "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"11100", "jgt":"11101", "je":"11111", "hlt":"11010"}
+
+# dict_opcodes = {"add":"00000", "sub":"00001", "mov":"00011", "ld":"00100",
+#         "st":"00101", "mul":"00110", "div":"00111", "rs":"01000",
+#         "ls":"01001", "xor":"01010", "or":"01011", "and":"01100",
+#         "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"11100", "jgt":"11101", "je":"11111", "hlt":"11010"}
 
 dict_reg = {"R0":"000", "R1":"001","R2":"010","R3":"011","R4":"100", "R5":"101","R6":"110", "FLAGS":"111"}
 
@@ -25,7 +25,15 @@ with open("input.txt", "r") as f:
                 break
 
             if i in dict_opcodes:
-                single_line += dict_opcodes[i]
+
+                if i == "mov":
+                    if line[-1][0] == '$':
+                        single_line += dict_opcodes[i][0]
+                    else:
+                        single_line += dict_opcodes[i][1]
+                else:
+                    single_line += dict_opcodes[i]
+
 
             if i in dict_reg:
                 single_line += dict_reg[i]
