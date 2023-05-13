dab4b167647a914679d1a91ec4e20d58da986a09
SGx3377 <souparno22506@iiitd.ac.in>
2023-05-13 20:23:53 +0530

    Update Souparno.py
    Error handling for Halt

--- Souparno.py
+++ Souparno.py
@@ -1,86 +1,18 @@
-dict_opcodes = {"add": "00000", "sub": "00001", "mov": ["00010", "00011"], "ld": "00100",
-                "st": "00101", "mul": "00110", "div": "00111", "rs": "01000",
-                "ls": "01001", "xor": "01010", "or": "01011", "and": "01100",
-                "not": "01101", "cmp": "01110", "jmp": "01111", "jlt": "11100", "jgt": "11101", "je": "11111",
-                "hlt": "11010"}
-
-# dict_opcodes = {"add":"00000", "sub":"00001", "mov":"00011", "ld":"00100",
-#         "st":"00101", "mul":"00110", "div":"00111", "rs":"01000",
-#         "ls":"01001", "xor":"01010", "or":"01011", "and":"01100",
-#         "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"11100", "jgt":"11101", "je":"11111", "hlt":"11010"}
-
-dict_reg = {"R0": "000", "R1": "001", "R2": "010", "R3": "011", "R4": "100", "R5": "101", "R6": "110", "FLAGS": "111"}
-dict_var = {}
-def dec_bin(dec):
-    dec = int(dec)
-    binary = ''
-    while dec != 0:
-        if dec % 2 == 0:
-            binary += '0'
-        if dec % 2 == 1:
-            binary += '1'
-        dec = dec // 2
-
-    unused = 7 - len(binary)
-    binary = binary[-1::-1]
-    binary = '0' * unused + binary
-    return binary
-
-
-with open("input.txt", "r") as f:
-    new_str = ''
-    count = 0
-    for lines in f.readlines():
-        a = lines.strip(' ')
-        b = a.strip('\t')
-        c = b.strip('\n')
-        #q = lines[:-1]
-        line = c.split(" ")
-        str_final = ''
-        single_line = ''
-
-        for i in line:
-            print(i)
-            if i == '':
+# Error handling for Halt
+# List to go in function is list_of_assembly_inst
+def halt_error(list):
+    l = list
+    np = 0
+    for i in range (len(l)):
+        if l[i][1]!='hlt':
+            np += 1
+        else:
+            if i!=len(l)-1:
+                print(f'Halt is not present at the right place, it is present in line {i+1}')
+                np = 0
                 break
-
-            if i in dict_opcodes:
-
-                if i == "mov":
-                    if line[-1][0] == '$':
-                        single_line += dict_opcodes[i][0]
-                        count += 1
-                    else:
-                        single_line += dict_opcodes[i][1]
-                        count += 1
-                else:
-                    single_line += dict_opcodes[i]
-                    count += 1
-
-            if i in dict_reg:
-                single_line += dict_reg[i]
-
-            if i[0] == '$':
-                single_line += dec_bin(i[1::1])
-
-        unused = 16 - len(single_line)
-        str_final = single_line[0:5] + ('0' * unused) + (single_line[5:])
-        new_str += str_final + '\n'
-    print(f'Number of instructions are {count}')
-    f.seek(0)
-    t = count
-    for lines in f.readlines():
-        a = lines.strip(' ')
-        b = a.strip('\t')
-        c = b.strip('\n')
-        #q = lines[:-1]
-        line = c.split(" ")
-        str_final = ''
-        single_line = ''
-        if line[0]=="var":
-            dict_var[line[1]] = dec_bin(t+1)
-            t += 1
-print(dict_var)
-# print(new_str)
-with open("output.txt", "w") as f:
-    f.write(new_str)
+            else:
+                np = 0
+                break
+    if np!=0:
+        print(f'Halt is not present')
