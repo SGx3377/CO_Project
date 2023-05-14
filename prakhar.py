5d83889fa806f5b9305f155fb7aab55f65bdacc0
VIRUSXSUJAL69 <prakhar22361@iiitd.ac.in>
2023-05-14 14:22:54 +0530

    ERRROR FOR B AND C
    created a function to check error for b and c and committed to the branch prakhar-agrawal

--- prakhar.py
+++ prakhar.py
@@ -1,65 +1,28 @@
-dict_opcodes = {"add":"00000", "sub":"00001", "mov":["00010","00011"], "ld":"00100",
-        "st":"00101", "mul":"00110", "div":"00111", "rs":"01000",
-        "ls":"01001", "xor":"01010", "or":"01011", "and":"01100",
-        "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"11100", "jgt":"11101", "je":"11111", "hlt":"11010"}
+def b_c(str1):
+    a = str1
+    for i in range(len(a)): #to remove all trailing white spaces
+        if a[i].isnumeric(): 
+            a = a[i::]
+            break
 
-# dict_opcodes = {"add":"00000", "sub":"00001", "mov":"00011", "ld":"00100",
-#         "st":"00101", "mul":"00110", "div":"00111", "rs":"01000",
-#         "ls":"01001", "xor":"01010", "or":"01011", "and":"01100",
-#         "not":"01101", "cmp":"01110", "jmp":"01111", "jlt":"11100", "jgt":"11101", "je":"11111", "hlt":"11010"}
+    list_opcodes = {"00000","00001","00010","00011","00100","00101", "00110", "00111","01000", "01001", "01010","01011", "01100",
+        "01101", "01110", "01111", "11100", "11101", "11111", "11010"}
 
-dict_reg = {"R0":"000", "R1":"001","R2":"010","R3":"011","R4":"100", "R5":"101","R6":"110", "FLAGS":"111"}
+    list2 = ['r0', 'r1','r2','r3','r4','r5','r6','r7']
+    list1 = a.split(' ')
+    
+    if list1[0] not in list_opcodes:
+        return "ERROR : OPCODE NOT IN THE GIVEN ISA"
 
-def dec_bin(dec):
-    dec = int(dec)
-    binary = ''
-    while (dec!=0):
-        if dec%2==0:
-            binary += '0'
-        if dec%2==1:
-            binary += '1'
-        dec = dec//2
+    if list1[2].isnumeric():
+        if int(list1[2])>127:
+            return 'ERROR : IMMIDIATE VALUE GREATER THAN 127'
 
-    unused = 7 - len(binary)
-    binary = binary[-1::-1]
-    binary = '0'*unused + binary
-    return binary
+        if list1[1].lower() not in list2:
+            return 'ERROR : REGISTER NOT IN THE GIVEN ISA'
+    else:
+        if list1[1].lower() not in list2 or list1[2].lower() not in list2:
+            return 'ERROR : REGISTER NOT IN THE GIVEN ISA'
 
-with open("input.txt", "r") as f:
-
-    new_str = ''
-    for lines in f.readlines():
-        lines.strip(' \n\t')
-        line = lines.split(" ")
-        str_final = ''
-        single_line = ''
-
-        for i in line:
-            # print(i)
-            if i == '':
-                break
-
-            if i in dict_opcodes:
-
-                if i == "mov":
-                    if line[-1][0] == '$':
-                        single_line += dict_opcodes[i][0]
-                    else:
-                        single_line += dict_opcodes[i][1]
-                else:
-                    single_line += dict_opcodes[i]
-
-
-            if i in dict_reg:
-                single_line += dict_reg[i]
-
-            if i[0] =='$' :
-                single_line += dec_bin(i[1::1])
-
-        unused = 16 - len(single_line)
-        str_final = single_line[0:5] + ('0'*unused) + (single_line[5:])
-        new_str += str_final + '\n'
-
-# print(new_str)
-with open("output.txt", "w") as f:
-    f.write(new_str)
+# print(b_c("  00111 R0 R1 "))
+# return none if no error
